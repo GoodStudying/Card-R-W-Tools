c5ca841c82db95b89e5702d4a3492dd656fd215e
ESXI <904342797@QQ.COM>
2026-01-20 20:48:36 +0800

    修正版本号错误格式

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "cn.ratnoumi.bcardtools"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1.0
+        versionCode = 1
         versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
