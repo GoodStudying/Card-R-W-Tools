4a809778bfc05b194d49955a1389faafd9e259cf
旺仔 <904342797@qq.com>
2026-01-20 17:24:21 +0800

    更新版本到V1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "cn.ratnoumi.bcardtools"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.1.0"
+        versionCode = 2.0
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
