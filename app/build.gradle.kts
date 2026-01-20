01d524b91d2a82c125cddf83c9a9ed710dd9f77d
ESXI <904342797@QQ.COM>
2026-01-20 20:30:58 +0800

    更新版本为v1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "cn.ratnoumi.bcardtools"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2.0
-        versionName = "1.2."
+        versionCode = 1.0
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
