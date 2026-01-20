8af748b287a71b514ae8cc6a3796428074a6dca0
ESXI <904342797@QQ.COM>
2026-01-20 20:49:18 +0800

    更新版本到v1.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = 1
-        versionName = "1.2.2"
+        versionName = "1.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
