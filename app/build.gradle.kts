c725fb28d5af43e1c45b1ca6c9b1b9d3370b846a
旺仔 <904342797@qq.com>
2026-01-22 10:26:45 +0800

    升级版本至 1.3.4 并添加 Gitee 发布附件同步

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "cn.ratnoumi.bcardtools"
         minSdk = 28
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.3.3"
+        versionCode = 11
+        versionName = "1.3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
